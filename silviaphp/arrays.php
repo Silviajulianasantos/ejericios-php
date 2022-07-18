ab8ac640a1d4b4465253ffa1d08bff4ce613d9cb
Silvia Juliana Santos Galan <102997342+Silviajulianasantos@users.noreply.github.com>
2022-07-18 16:34:47 -0500

    Add files via upload

--- silviaphp/arrays.php
+++ silviaphp/arrays.php
@@ -1,26 +1,58 @@
 <?php
-  
-  //arrays
-  $array1[] = "posicin  1";
-  $array1[] = "posicion 2";
-  $array1[] = "posicion 3";
 
-  // imprimir array1
+    // arrays
+    $array1[] = "posicion 1";
+    $array1[] = "posicion 2";
+    $array1[] = "posicion 3";
 
-  echo $array1[2];
+    var_dump($array1);
 
-  var_dump($array1);
-  
-  // arrays
-  $array2 = array(
-    '0' => "a",
-    '1' => "b",
-    '2' => "c",
-  );
 
-  var_dump("*********************");
 
-  var_dump($array2);
+    $array2 = array(
+        '0' => 'a',
+        '1' => 'b',
+        '2' => 'c',
+    );
 
+
+    var_dump($array2);
+
+
+    //array asociativos
+    echo "<br/>";
+    echo "Array asociativos";
+
+    $arrayAsociativo = array(
+        'materia' => "Física",
+        'nota' => 3.0,
+        'nombre' => "Sivia",
+        'apellido' => "Santos",
+    );
+
+    var_dump($arrayAsociativo);
+    echo $arrayAsociativo["materia"];
+
+
+    echo "<br/>";
+    echo "<br/>";
+    echo "<br/>";
+    echo "<br/>";
+    echo "<br/>";
+    echo "<br/>";
+
+
+
+    //array multimensional
+    $notas = array(
+        '0' => array('materia' => "Física", "nombre" => "Fernando", "nota" => 3.5),
+        '1' => array('materia' => "Física", "nombre" => "Diana", "nota" => 4.0),
+        '2' => array('materia' => "Física", "nombre" => "Lucia", "nota" => 3.0),
+        '3' => array('materia' => "Física", "nombre" => "Juan", "nota" => 4.5),
+    );
+
+    var_dump($notas);
+
+    echo "Nombre de la nota mas alta:" . $notas[3]["nombre"];
 
 ?>
